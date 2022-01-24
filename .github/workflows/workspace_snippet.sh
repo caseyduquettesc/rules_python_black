6db43ecbc15aea2a4aa28a638923d560e0f79de8
Casey Duquette <casey.duquette@snapchat.com>
2022-01-23 20:50:59 -0800

    build(init): Initial commit

--- .github/workflows/workspace_snippet.sh
+++ .github/workflows/workspace_snippet.sh
@@ -5,7 +5,7 @@ set -o errexit -o nounset -o pipefail
 # Set by GH actions, see
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 TAG=${GITHUB_REF_NAME}
-PREFIX="rules_mylang-${TAG:1}"
+PREFIX="rules_python_black-${TAG:1}"
 SHA=$(git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip | shasum -a 256 | awk '{print $1}')
 
 cat << EOF
@@ -13,19 +13,19 @@ WORKSPACE snippet:
 \`\`\`starlark
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 http_archive(
-    name = "com_myorg_rules_mylang",
+    name = "rules_python_black",
     sha256 = "${SHA}",
     strip_prefix = "${PREFIX}",
-    url = "https://github.com/myorg/rules_mylang/archive/${TAG}.tar.gz",
+    url = "https://github.com/caseyduquettesc/rules_python_black/archive/${TAG}.tar.gz",
 )
 
-# Fetches the rules_mylang dependencies.
+# Fetches the rules_python_black dependencies.
 # If you want to have a different version of some dependency,
 # you should fetch it *before* calling this.
 # Alternatively, you can skip calling this function, so long as you've
 # already fetched all the dependencies.
-load("@com_myorg_rules_mylang//mylang:dependencies.bzl", "rules_mylang_dependencies")
-rules_mylang_dependencies()
+load("@rules_python_black//python_black:repositories.bzl", "rules_python_black_dependencies")
+rules_python_black_dependencies()
 
 \`\`\`
 EOF
